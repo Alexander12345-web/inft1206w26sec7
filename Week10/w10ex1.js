d31b333ce21a9f30b201ca2dffc0277ab6e56cd5
Alexander12345-web <big_ben13@outlook.com>
2026-04-01 14:37:41 -0400

    Tutorial code

---
+++ Week10/w10ex1.js
@@ -0,0 +1,13 @@
+console.log("Test 1")
+
+document.querySelector('#id1').addEventListener('mouseover',function(){
+    this.style.color="green";
+})
+
+document.querySelector('#id1').addEventListener('mouseout',function(){
+    this.style.color="black";
+})
+
+document.querySelector('#id1').onclick=function(){
+    this.textContent = "Hello"
+}
