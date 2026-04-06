6ba4069725862bc7d73791f08b3cb09eb99ca10f
Alexander12345-web <big_ben13@outlook.com>
2026-04-06 17:46:33 -0400

    Imported images, created a constant to call images, created a base URL for images, Created for loop

--- Assignment4/Part2/p2.js
+++ Assignment4/Part2/p2.js
@@ -3,3 +3,32 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Constant for the images
+const images = [
+    { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+    { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+    { filename: "pic3.jpg", alt: "Purple and white pansies" },
+    { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+    { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+// Base URL for the images
+const baseURL =
+    "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+// Loop through the images
+for (const image of images) {
+
+    const newImage = document.createElement("img");
+    newImage.src = `${baseURL}${image.filename}`;
+    newImage.alt = image.alt;
+    newImage.tabIndex = "0";
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", updateDisplayedImage);
+    newImage.addEventListener("keydown", (e) => {
+        if (e.code === "Enter") {
+            updateDisplayedImage(e);
+        }
+    });
+}
